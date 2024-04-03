1500237969f69858ffa170585ed26a9f5a51592f
Tan Pham <xuantan.pham2808@gmail.com>
2024-04-03 17:33:56 +0700

    CRUD Product theo chuẩn CQRS - Carten framework: Xây dựng End Point nhẹ và hiệu quả => ICarterModule - Marten Lib: Xây dựng kết nối với PostgreSQL

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    public class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById
+{
+    // Request query có param
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdQueryHandler
+        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
