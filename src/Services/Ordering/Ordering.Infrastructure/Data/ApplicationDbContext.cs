5502b92c8d4c6b9a3e140b31212a431bc2175f9c
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-05-27 20:53:54 +0700

    Khởi tạo cấu hình (config Entity)

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    // Các table trong database
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        /* ApplyConfigurationsFromAssembly là một phương thức của ModelBuilder. 
+         * Nó sẽ tìm và áp dụng tất cả các cấu hình từ assembly hiện tại 
+         * (assembly là một khái niệm trong .NET, đại diện cho một tập hợp các mã đã biên dịch, như một file DLL).*/
+
+        /* Dòng này sẽ tìm kiếm và áp dụng tất cả các lớp cấu hình thực thể (implement IEntityTypeConfiguration<TEntity>) trong assembly hiện tại. 
+         * Các lớp cấu hình này định nghĩa chi tiết về các thực thể như bảng nào sẽ được tạo ra, các cột, khóa chính, khóa ngoại, chỉ mục, v.v*/
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,131 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id).HasConversion(
+                        orderId => orderId.Value,
+                        dbId => OrderId.Of(dbId));
+
+        /* Mỗi Order phải có một Customer và 
+         * Một Customer có thể có nhiều Order.*/
+        builder.HasOne<Customer>()
+          .WithMany()
+          .HasForeignKey(o => o.CustomerId)
+          .IsRequired();
+
+        /* Mỗi Order có thể có nhiều OrderItem và 
+         * Mỗi OrderItem phải thuộc về một Order cụ thể. */
+        builder.HasMany(o => o.OrderItems)
+            .WithOne()
+            .HasForeignKey(oi => oi.OrderId);
+
+        /* Cấu hình thuộc tính phức hợp OrderName của thực thể Order, 
+         * đảm bảo rằng cột tương ứng trong cơ sở dữ liệu sẽ có tên là OrderName
+         * có độ dài tối đa 100 ký tự và không cho phép giá trị null. 
+         * Thuộc tính phức hợp này có một thuộc tính con là Value.*/
+        builder.ComplexProperty(
+            o => o.OrderName, nameBuilder =>
+            {
+                nameBuilder.Property(n => n.Value)
+                    .HasColumnName(nameof(Order.OrderName))
+                    .HasMaxLength(100)
+                    .IsRequired();
+            });
+
+        builder.ComplexProperty(
+           o => o.ShippingAddress, addressBuilder =>
+           {
+               addressBuilder.Property(a => a.FirstName)
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+               addressBuilder.Property(a => a.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+               addressBuilder.Property(a => a.EmailAddress)
+                   .HasMaxLength(50);
+
+               addressBuilder.Property(a => a.AddressLine)
+                   .HasMaxLength(180)
+                   .IsRequired();
+
+               addressBuilder.Property(a => a.Country)
+                   .HasMaxLength(50);
+
+               addressBuilder.Property(a => a.State)
+                   .HasMaxLength(50);
+
+               addressBuilder.Property(a => a.ZipCode)
+                   .HasMaxLength(5)
+                   .IsRequired();
+           });
+
+        builder.ComplexProperty(
+          o => o.BillingAddress, addressBuilder =>
+          {
+              addressBuilder.Property(a => a.FirstName)
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+              addressBuilder.Property(a => a.LastName)
+                   .HasMaxLength(50)
+                   .IsRequired();
+
+              addressBuilder.Property(a => a.EmailAddress)
+                  .HasMaxLength(50);
+
+              addressBuilder.Property(a => a.AddressLine)
+                  .HasMaxLength(180)
+                  .IsRequired();
+
+              addressBuilder.Property(a => a.Country)
+                  .HasMaxLength(50);
+
+              addressBuilder.Property(a => a.State)
+                  .HasMaxLength(50);
+
+              addressBuilder.Property(a => a.ZipCode)
+                  .HasMaxLength(5)
+                  .IsRequired();
+          });
+
+        builder.ComplexProperty(
+               o => o.Payment, paymentBuilder =>
+               {
+                   paymentBuilder.Property(p => p.CardName)
+                       .HasMaxLength(50);
+
+                   paymentBuilder.Property(p => p.CardNumber)
+                       .HasMaxLength(24)
+                       .IsRequired();
+
+                   paymentBuilder.Property(p => p.Expiration)
+                       .HasMaxLength(10);
+
+                   paymentBuilder.Property(p => p.CVV)
+                       .HasMaxLength(3);
+
+                   paymentBuilder.Property(p => p.PaymentMethod);
+               });
+
+        builder.Property(o => o.Status)
+            .HasDefaultValue(OrderStatus.Draft)
+            .HasConversion(
+                s => s.ToString(),
+                dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus));
+
+        builder.Property(o => o.TotalPrice);
+    }
+}
