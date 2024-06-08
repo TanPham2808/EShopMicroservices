e574f1044080884b7d1dd340293e8dd347afe0af
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-06-08 14:22:33 +0700

    HealthCheck SQL Server

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,20 @@
 ﻿using BuildingBlocks.Exceptions.CustomException;
 using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
         services.AddExceptionHandler<CustomExceptionHandler>();  // Tùy chỉnh Exception Response (Nhớ add thêm app.UseExceptionHandler)
+
+        // Thêm tính năng kiểm tra tình trạng sức khỏe của services
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!); 
 
         return services;
     }
@@ -18,6 +24,12 @@ public static class DependencyInjection
         app.MapCarter();
         app.UseExceptionHandler(options => { });  // Hanlde tùy chỉnh Exception Response (Đi 1 cặp chung với method AddExceptionHandler)
 
+        // Kích hoạt tính năng kiểm tra tình trạng sức khỏe của services
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            }); 
         return app;
     }
 }
