389aee56cf55d2f4c81be7564aec4cb825995572
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-06-24 22:11:33 +0700

    Chỉnh sửa lại docker file. Tích hợp để chạy docker

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -18,7 +18,8 @@ internal class InitialData
             Product.Create(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), "IPhone X", 500),
             Product.Create(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), "Samsung 10", 400),
             Product.Create(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), "Huawei Plus", 650),
-            Product.Create(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), "Xiaomi Mi", 450)
+            Product.Create(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), "Xiaomi Mi", 450),
+            Product.Create(ProductId.Of(new Guid("9c6767bc-0472-4c81-8808-ea40685cec57")), "New IPhone Tan", 800)
         };
 
     public static IEnumerable<Order> OrdersWithItems
