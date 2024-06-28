4b3622ae93a1bc0546b786c462f7ebc3ca92e811
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-06-28 22:49:19 +0700

    Thêm Refit.HttpClientFactory Thêm Interface sử dụng Refit

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -5,7 +5,7 @@ using MediatR;
 
 namespace Basket.API.Basket.GetBasket;
 
-//public record GetBasketRequest(string UserName); 
+public record GetBasketRequest(string UserName); 
 public record GetBasketResponse(ShoppingCart Cart);
 
 public class GetBasketEndpoints : ICarterModule
--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -1,7 +1,27 @@
+﻿using Refit;
+using Shopping.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Cấu hình Refit sử dụng Interface ICatalogService, IBasketService, IOrderingService
+builder.Services.AddRefitClient<ICatalogService>()
+     .ConfigureHttpClient(c =>
+     {
+         c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+     });
+builder.Services.AddRefitClient<IBasketService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
+builder.Services.AddRefitClient<IOrderingService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
 
 var app = builder.Build();
 
